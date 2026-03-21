f49077634e600d50b85585bef6599740020d601f
sahilchauhan2105 <92661060+sahilchauhan2105@users.noreply.github.com>
2026-03-21 22:16:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = "fx-calculator-v1";
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/forex-calculator/",
-        "/forex-calculator/index.html",
-        "/forex-calculator/manifest.json",
-        "/forex-calculator/icon.png"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  console.log("Service Worker Installed");
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", () => {
+  // No caching (safe mode)
 });
